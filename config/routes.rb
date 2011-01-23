688bbfb65ec798266f14e8e60f653330869e3420
Colin Nederkoorn <colin.nederkoorn@gmail.com>
2011-01-23 13:09:52 -0500

    Moved the application template to partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Reservations::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
 
   root :to => "pages#home"
 
