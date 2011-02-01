e68256ce058aa8d4cff1fc6c80160c846979c7e0
Colin Nederkoorn <colin.nederkoorn@gmail.com>
2011-02-01 10:37:38 -0500

    Fixed failing tests for reservations

--- spec/controllers/reservations_controller_spec.rb
+++ spec/controllers/reservations_controller_spec.rb
@@ -9,27 +9,27 @@ describe ReservationsController do
       @property = Factory(:property)
       @attr = {   :email => "colin@blah.com", 
                   :from_date => Time.now + 5.days,
-                  :to_date => Time.now + 6.days,
-                  :property_id => "1" 
+                  :to_date => Time.now + 6.days
       }
     end
 
     describe "failure" do
       before(:each) do
-        @attr.merge(:to_date => "")
+        @reservation = Factory.build(:reservation, :to_date => "", :property => @property)
       end
 
       it "should not create a reservation" do
         lambda do
-          post :create, [@property, @reservation] => @attr
+          post :create, :reservation => @reservation.attributes, :property_id => @property.id
         end.should_not change(Reservation, :count)
       end
     
     end
     describe "success" do
       it "should create a reservation" do
-        post :create, [:reservation] => @attr
-        response.should be_success
+        lambda do
+          post :create, :reservation => @attr, :property_id => @property.id
+        end.should change(Reservation, :count).by(1)
       end
     end
   end
